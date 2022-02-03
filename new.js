8ef28af7faeb448a526fb18df931284bfb5f1b1a
Hermann-Kateu <kateuhermann3@gmail.com>
2022-02-03 14:42:35 +0100

    Additon of new.css new.html new.js

---
+++ new.js
@@ -0,0 +1,32 @@
+function adding(x,y){
+    var x,y;
+    return x+y;
+}
+var x='When sending the alert message the varaible should not contain qoutations';
+alert("The addition of X and Y is:" +adding(3,4))
+
+for(let i=0; i<10; i++){
+   let number=[];
+   number.push(i)
+}
+ function clickMe(){
+     alert('BRO STOP CLICKING ME ABEG!')
+ }
+ 
+ //another 
+
+ function sayHello(){
+    for(let i=0; i<5; i++){
+        console.log(i)
+    }
+ }
+ sayHello();
+ //
+
+ const person={
+     name:'john',
+     walk(){
+         console.log(this)
+     }
+}
+person.walk();
